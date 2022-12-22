54584f0fdfef4f30385657bdb168707d45c0958e
Ayeon <dldkekdusz@naver.com>
2022-12-22 16:36:07 +0900

    [feat] BaseViewController 화면전환 공통 로직 적용

--- SharePlanner/Base/BaseViewController.swift
+++ SharePlanner/Base/BaseViewController.swift
@@ -9,22 +9,14 @@ import Foundation
 import RxSwift
 import ReactorKit
 
-
+//MARK: - TransitionType
 enum TransitionType {
     static let defaultDimmColor = UIColor.black.withAlphaComponent(0.6)
     
     case push(enableGesturePop: Bool = false)
     case dimmPresent(from: PresentationDirection = .bottom, dimmColor: UIColor = TransitionType.defaultDimmColor)
     case present
-    case tabBarPresent(dimmColor: UIColor = TransitionType.defaultDimmColor)
-    case naviPresent(dimmColor: UIColor = TransitionType.defaultDimmColor)
-}
-
-enum PresentationDirection {
-    case left
-    case top
-    case right
-    case bottom
+    case presentNavigation(dimmColor: UIColor = TransitionType.defaultDimmColor)
 }
 
 //MARK: - Bind Reactor Action And State
@@ -42,16 +34,21 @@ extension BindReactorActionStateProtocol {
     }
 }
 
+
 //MARK: - View Protocol
 protocol ConfigureViewProtocol {
     func configureLayout()
     func configureLayer()
 }
 
+
+// MARK: - SuperClass of Common ViewControllers
 typealias BaseViewController<T: Reactor> = BaseViewControllerClass<T> & BindReactorActionStateProtocol
 
 class BaseViewControllerClass<T: Reactor>: UIViewController, ConfigureViewProtocol {
     typealias ReactorType = T
+    
+    lazy var dimmTransitionDelegate = DimmPresentManager()
     
     var disposeBag = DisposeBag()
     
@@ -92,3 +89,153 @@ class BaseViewControllerClass<T: Reactor>: UIViewController, ConfigureViewProtoc
     func configureLayer() {}
 }
 
+extension BaseViewControllerClass {
+    ///Move To Scene
+    func transition(to scene: Scene, using style: TransitionType, animated: Bool, completion: (() -> Void)? = nil) {
+        let target = scene.instantiate()
+        self.moveToTarget(to: target, using: style, animated: animated, completion: completion)
+    }
+    
+    ///Move To ViewController
+    func transition(to vc: UIViewController, using style: TransitionType, animated: Bool, completion: (() -> Void)? = nil) {
+        self.moveToTarget(to: vc, using: style, animated: animated, completion: completion)
+    }
+    
+    ///Move Action
+    func moveToTarget(to vc: UIViewController, using style: TransitionType, animated: Bool, completion: (() -> Void)? = nil) {
+        let target = vc
+        
+        if var baseVC = target as? Self {
+            baseVC.transitionType = style
+        }
+        
+        switch style {
+        case .push:
+            guard let nav = self.navigationController else { return }
+            target.hidesBottomBarWhenPushed = true
+            nav.pushViewController(target, animated: animated, completion: completion)
+            
+        case .dimmPresent(let from, let dimmColor):
+            dimmTransitionDelegate.direction = from
+            dimmTransitionDelegate.dimmColor = dimmColor
+            target.modalPresentationStyle = .custom
+            target.transitioningDelegate = dimmTransitionDelegate
+            
+            self.present(target, animated: animated, completion: completion)
+            
+        case .present:
+            self.present(target, animated: animated, completion: completion)
+            
+        case .presentNavigation:
+            let navTarget = UINavigationController(rootViewController: target)
+            navTarget.modalPresentationStyle = .overCurrentContext
+            navTarget.setNavigationBarHidden(true, animated: false)
+            if let tabBarController = self.tabBarController {
+                tabBarController.present(navTarget, animated: animated, completion: completion)
+            } else {
+                self.present(navTarget, animated: animated, completion: completion)
+            }
+        }
+    }
+
+    ///Close ViewController And Move To ViewController
+    func transitionAfterClose(to vc: UIViewController, using style: TransitionType, animated: Bool, completion: (() -> Void)? = nil) {
+        
+        if let rootNav = self.navigationController {
+            self.close(animated: true) {
+                let prevVC = rootNav.topViewController as? Self
+                prevVC?.transition(to: vc, using: style, animated: animated, completion: completion)
+            }
+        } else {
+            if let presentingVC = self.presentingViewController {
+                if let pvc = presentingVC as? Self {
+                    self.close(animated: true) {
+                        pvc.transition(to: vc, using: style, animated: animated, completion: completion)
+                    }
+                } else {
+                    var prevNavigation: UINavigationController?
+                    if let prevNav = presentingVC as? UINavigationController {
+                        prevNavigation = prevNav
+                    } else if let prevTabBar = presentingVC as? UITabBarController {
+                        prevNavigation = prevTabBar.viewControllers?[prevTabBar.selectedIndex] as? UINavigationController
+                    }
+                    
+                    if let prevNav = prevNavigation?.topViewController,
+                       let prevVC = prevNav as? Self {
+                        self.close(animated: true) {
+                            prevVC.transition(to: vc, using: style, animated: animated, completion: completion)
+                        }
+                    }
+                    
+                }
+            }
+        }
+    }
+    
+    ///Close ViewController And Move To Scene
+    func transitionAfterClose(to scene: Scene, using style: TransitionType, animated: Bool, completion: (() -> Void)? = nil) {
+        let vc = scene.instantiate()
+        
+        if let rootNav = self.navigationController {
+            self.close(animated: true) {
+                let prevVC = rootNav.topViewController as? Self
+                prevVC?.transition(to: vc, using: style, animated: animated, completion: completion)
+            }
+        } else {
+            if let presentingVC = self.presentingViewController {
+                if let pvc = presentingVC as? Self {
+                    self.close(animated: true) {
+                        pvc.transition(to: vc, using: style, animated: animated, completion: completion)
+                    }
+                } else {
+                    var prevNavigation: UINavigationController?
+                    if let prevNav = presentingVC as? UINavigationController {
+                        prevNavigation = prevNav
+                    } else if let prevTabBar = presentingVC as? UITabBarController {
+                        prevNavigation = prevTabBar.viewControllers?[prevTabBar.selectedIndex] as? UINavigationController
+                    }
+                    
+                    if let prevNav = prevNavigation?.topViewController,
+                       let prevVC = prevNav as? Self {
+                        self.close(animated: true) {
+                            prevVC.transition(to: vc, using: style, animated: animated, completion: completion)
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    ///Close ViewController By TransitionType Type
+    func close(animated: Bool, completion: (() -> Void)? = nil) {
+        switch self.transitionType {
+        case .push:
+            self.navigationController?.popViewController(animated: animated, completion: completion)
+        case .present, .dimmPresent, .presentNavigation:
+            self.dismiss(animated: animated, completion: completion)
+        default:
+            if let nav = self.navigationController {
+                nav.popViewController(animated: animated)
+            } else if let _ = self.presentingViewController {
+                self.dismiss(animated: animated, completion: completion)
+            }
+        }
+    }
+    
+    ///Close Presenting NavigationController
+    func closePresentedNavigation(animated: Bool, completion: (() -> Void)? = nil) {
+        self.navigationController?.dismiss(animated: animated, completion: completion)
+    }
+    
+    ///Move To RootViweController
+    func moveToRoot(animated: Bool, completion: (() -> Void)? = nil) {
+        DispatchQueue.main.async {
+            if let rootNav = UIApplication.shared.windows.first?.rootViewController?.navigationController {
+                rootNav.dismiss(animated: animated) {
+                    rootNav.popToRootViewController(animated: animated)
+                    completion?()
+                }
+            }
+        }
+    }
+}
